5bae61ce996e40e7cac8a49b3d7b13e9374787d5
BeomBeom2 <whwjdqja20@naver.com>
2025-12-17 23:57:45 +0900

    [Add] 시스템 자체 스플래쉬 제거 및 KusTheme 프레임 코드 생성

---
+++ composeApp/src/desktopMain/kotlin/com/kus/kustaurant/theme/Theme.desktop.kt
@@ -0,0 +1,9 @@
+package com.kus.kustaurant.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun platformDynamicColorScheme(
+    darkTheme: Boolean
+): ColorScheme? = null
