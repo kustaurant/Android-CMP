2ac5f22703ec656f06b3168d6a027683e7ff0edc
BeomBeom2 <whwjdqa20@naver.com>
2026-02-01 22:45:43 +0900

    [Chore] 디자인 테마 적용

--- composeApp/src/desktopMain/kotlin/com/kus/kustaurant/navigation/BottomBarIconPainters.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/kus/kustaurant/navigation/BottomBarIconPainters.desktop.kt
@@ -2,17 +2,17 @@ package com.kus.kustaurant.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
-import kustaurant.feature.community.generated.resources.ic_community
-import kustaurant.feature.draw.generated.resources.ic_draw
-import kustaurant.feature.home.generated.resources.ic_home
-import kustaurant.feature.my.generated.resources.ic_my
-import kustaurant.feature.tier.generated.resources.ic_tier
+import kustaurant.shared.feature.community.generated.resources.ic_community
+import kustaurant.shared.feature.draw.generated.resources.ic_draw
+import kustaurant.shared.feature.home.generated.resources.ic_home
+import kustaurant.shared.feature.my.generated.resources.ic_my
+import kustaurant.shared.feature.tier.generated.resources.ic_tier
 import org.jetbrains.compose.resources.painterResource
-import kustaurant.feature.home.generated.resources.Res as HomeRes
-import kustaurant.feature.draw.generated.resources.Res as DrawRes
-import kustaurant.feature.tier.generated.resources.Res as TierRes
-import kustaurant.feature.community.generated.resources.Res as CommunityRes
-import kustaurant.feature.my.generated.resources.Res as MyRes
+import kustaurant.shared.feature.home.generated.resources.Res as HomeRes
+import kustaurant.shared.feature.draw.generated.resources.Res as DrawRes
+import kustaurant.shared.feature.tier.generated.resources.Res as TierRes
+import kustaurant.shared.feature.community.generated.resources.Res as CommunityRes
+import kustaurant.shared.feature.my.generated.resources.Res as MyRes
 
 @Composable
 actual fun homeTabPainter(): Painter = painterResource(HomeRes.drawable.ic_home)
