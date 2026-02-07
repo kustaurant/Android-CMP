788444fdd1907d57fbb887fe8f88d46ae18a939f
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-02-07 20:23:34 +0900

    [ui/#13] 상세화면 상단 그라데이션 레잉아웃 추가 및 컴포넌트화

--- shared/feature/detail/src/commonMain/kotlin/com/kus/feature/detail/ui/DetailScreen.kt
+++ shared/feature/detail/src/commonMain/kotlin/com/kus/feature/detail/ui/DetailScreen.kt
@@ -1,24 +1,19 @@
-package com.kus.feature.detail.ui
+﻿package com.kus.feature.detail.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.kus.feature.detail.component.DetailHeaderImage
 import com.kus.feature.detail.component.DetailRestInfo
 import com.kus.feature.detail.component.DetailTabSection
-import kustaurant.shared.feature.detail.generated.resources.Res
-import kustaurant.shared.feature.detail.generated.resources.img_rest_example
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun DetailScreen() {
@@ -36,12 +31,8 @@ fun DetailScreen() {
             Box(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Image(
-                    painter = painterResource(Res.drawable.img_rest_example),
-                    contentDescription = "식당 이미지 사진",
-                    modifier = Modifier.fillMaxWidth()
-                        .height(imageHeight),
-                    contentScale = ContentScale.Crop
+                DetailHeaderImage(
+                    imageHeight = imageHeight
                 )
 
                 DetailRestInfo(
