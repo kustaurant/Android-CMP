b7f27ae458631fd069cf0869a797acf04f57afe1
BeomBeom2 <whwjdqa20@naver.com>
2026-02-09 22:17:35 +0900

    [Add] serialization 모듈 추가

---
+++ shared/core/serialization/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget()
+
+    val xcfName = "shared:core:serializationKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    jvm("desktop")
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(libs.kotlinx.serialization.json)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.kus.core.serialization"
+
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
