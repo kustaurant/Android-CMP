323d09e09aa576c6031c1c255023365836198746
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-02-22 11:48:14 +0900

    [chore/#13] uiShowcase 크기 영역 표준 핸드폰 변경

---
+++ uiShowcase/src/desktopMain/kotlin/com/kus/designSystemShowcase/component/EvaluatePreviewScreen.kt
@@ -0,0 +1,9 @@
+package com.kus.designSystemShowcase.component
+
+import androidx.compose.runtime.Composable
+import com.kus.feature.evaluate.ui.EvaluateScreen
+
+@Composable
+fun EvaluatePreviewScreen() {
+    EvaluateScreen()
+}
--- uiShowcase/src/desktopMain/kotlin/com/kus/designSystemShowcase/main.kt
+++ uiShowcase/src/desktopMain/kotlin/com/kus/designSystemShowcase/main.kt
@@ -1,14 +1,26 @@
 package com.kus.designSystemShowcase
 
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.kus.designSystemShowcase.component.DetailPreviewScreen
+import com.kus.designSystemShowcase.component.EvaluatePreviewScreen
+import com.kus.designsystem.theme.KusTheme
 
 // ./gradlew :uiShowcase:run
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "DesignSystem Showcase") {
-        MaterialTheme {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "DesignSystem Showcase",
+        state = rememberWindowState(
+            width = 360.dp,
+            height = 800.dp
+        )
+    ) {
+        KusTheme {
+            DetailPreviewScreen()
         }
     }
 }
