30c1021d7fdc38e7502aea05afa565b4d1c3a968
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-02-22 20:08:19 +0900

    [feat/#13] 평가하기 이미지 업로드 영역 플랫폼별 분리 추가
    - desktop 미지원 및 미사용에 따른 플랫폼 별 이미지 추가 컴포넌트 분리

--- shared/feature/evaluate/src/commonMain/kotlin/com/kus/feature/evaluate/component/EvaluationImage.kt
+++ shared/feature/evaluate/src/commonMain/kotlin/com/kus/feature/evaluate/component/EvaluationImage.kt
@@ -1,71 +1,10 @@
-//package com.kus.feature.evaluate.component
-//
-//import androidx.compose.foundation.Image
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.border
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.aspectRatio
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.layout.height
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.shape.RoundedCornerShape
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.draw.clip
-//import androidx.compose.ui.graphics.vector.ImageVector
-//import androidx.compose.ui.layout.ContentScale
-//import androidx.compose.ui.unit.dp
-//import com.kus.designsystem.theme.KusTheme
-//
-//@Composable
-//fun EvaluationImage(
-//
-//) {
-//    Box(
-//        modifier = Modifier.fillMaxWidth()
-//            .padding(horizontal = 20.dp)
-//            .padding(top = 30.dp)
-//            .border(
-//                shape = RoundedCornerShape(16.dp),
-//                width = 1.dp,
-//                color = KusTheme.colors.c_AAAAAA
-//            )
-//    ) {
-//        if (imageUri == null) {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(vertical = 20.dp),
-//                horizontalAlignment = Alignment.CenterHorizontally
-//            ) {
-//                Icon(
-//                    imageVector = ImageVector.vectorResource(ic_no_image),
-//                    contentDescription = null,
-//                    tint = TogedyTheme.colors.gray300
-//                )
-//
-//                Text(
-//                    text = "이미지 추가",
-//                    style = TogedyTheme.typography.body12m.copy(
-//                        color = TogedyTheme.colors.gray500
-//                    ),
-//                    modifier = Modifier.padding(top = 8.dp)
-//                )
-//            }
-//        } else {
-//            Image(
-//                painter = rememberAsyncImagePainter(model = imageUri),
-//                contentDescription = "선택된 스터디 이미지",
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .aspectRatio(16f / 9f),
-//                contentScale = ContentScale.Crop
-//            )
-//        }
-//    }
-//
-//}
+package com.kus.feature.evaluate.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun EvaluationImage(
+    imageUrl: String,
+    imageBytes: ByteArray?,
+    onImageSelected: (ByteArray) -> Unit,
+)
