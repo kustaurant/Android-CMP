43581908087a62ffb1192e72ef480fb6539cc053
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-02-25 23:41:02 +0900

    [ui/#13] 하단 평가 제출 바 추가 및 여백 추가

--- shared/feature/evaluate/src/commonMain/kotlin/com/kus/feature/evaluate/ui/EvaluateScreen.kt
+++ shared/feature/evaluate/src/commonMain/kotlin/com/kus/feature/evaluate/ui/EvaluateScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,12 +18,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kus.designsystem.component.KusButton
 import com.kus.designsystem.component.KusTopBar
 import com.kus.designsystem.theme.KusTheme
 import com.kus.designsystem.util.noRippleClickable
 import com.kus.feature.evaluate.component.EvaluationImage
-import com.kus.feature.evaluate.component.EvaluationRestInfoCard
 import com.kus.feature.evaluate.component.EvaluationKeyword
+import com.kus.feature.evaluate.component.EvaluationRestInfoCard
 import com.kus.feature.evaluate.component.EvaluationReview
 import com.kus.feature.evaluate.component.EvaluationStar
 import kustaurant.shared.core.designsystem.generated.resources.Res
@@ -37,6 +40,7 @@ fun EvaluateScreen(
     val uiState by viewModel.uiState.collectAsState()
     val restaurant = uiState.restaurant
     val evaluation = uiState.evaluation
+
     Box(
         modifier = Modifier.fillMaxSize()
             .background(color = KusTheme.colors.c_FFFFFF)
@@ -88,6 +92,13 @@ fun EvaluateScreen(
                 )
             }
 
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .height(72.dp)
+                )
+            }
+
         }
 
         Column(
@@ -114,6 +125,19 @@ fun EvaluateScreen(
             HorizontalDivider(
                 thickness = 1.dp,
                 color = KusTheme.colors.c_EAEAEA
+            )
+        }
+
+        Box(
+            modifier = Modifier.background(color = KusTheme.colors.c_FFFFFF)
+                .padding(horizontal = 20.dp, vertical = 10.dp)
+                .align(Alignment.BottomCenter),
+        ) {
+            KusButton(
+                enabled = true,
+                buttonName = "평가 제출하기",
+                roundedCornerShape = RoundedCornerShape(50.dp),
+                onClick = {}
             )
         }
     }
