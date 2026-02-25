8d189e49853914e9e2a77c3028e9cc738e7ad043
Chaerin Kim <rinring105@gmail.com>
2026-02-25 22:51:47 +0900

    [mod/#23] 검색어 일괄 삭제 아이콘 tint값 변경

--- shared/feature/search/src/commonMain/kotlin/com/kus/feature/search/component/KusSearchBox.kt
+++ shared/feature/search/src/commonMain/kotlin/com/kus/feature/search/component/KusSearchBox.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.kus.designsystem.theme.KusTheme
 import com.kus.designsystem.util.noRippleClickable
@@ -77,7 +78,7 @@ internal fun KusSearchBox(
                         .padding(start = 13.dp, end = 6.dp)
                         .size(20.dp)
                         .noRippleClickable { onValueChange("") },
-                    tint = KusTheme.colors.c_AAAAAA,
+                    tint = Color.Unspecified,
                 )
             }
 
