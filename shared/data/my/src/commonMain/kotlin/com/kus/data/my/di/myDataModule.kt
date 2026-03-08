64d24a6c511b51aa3581126a98a8a5a3052662f7
Chaerin Kim <rinring105@gmail.com>
2026-03-08 17:28:10 +0900

    [feat/#22] repository <-> impl 의존성 추가

--- shared/data/my/src/commonMain/kotlin/com/kus/data/my/di/myDataModule.kt
+++ shared/data/my/src/commonMain/kotlin/com/kus/data/my/di/myDataModule.kt
@@ -1,13 +1,24 @@
 package com.kus.data.my.di
 
+import MyRepository
 import com.kus.data.my.api.MyApi
 import com.kus.data.my.api.MyCommunityApi
 import com.kus.data.my.api.MyRestaurantApi
+import com.kus.data.my.repositoryimpl.MyCommunityRepositoryImpl
+import com.kus.data.my.repositoryimpl.MyRepositoryImpl
+import com.kus.data.my.repositoryimpl.MyRestaurantRepositoryImpl
+import com.kus.shared.domain.my.repository.MyCommunityRepository
+import com.kus.shared.domain.my.repository.MyRestaurantRepository
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val myDataModule = module {
     singleOf(::MyApi)
     singleOf(::MyCommunityApi)
     singleOf(::MyRestaurantApi)
+
+    singleOf(::MyRepositoryImpl) bind MyRepository::class
+    singleOf(::MyCommunityRepositoryImpl) bind MyCommunityRepository::class
+    singleOf(::MyRestaurantRepositoryImpl) bind MyRestaurantRepository::class
 }
