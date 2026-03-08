edf965aa843803b690db69c64a5fddcb96d73395
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-03-08 16:10:00 +0900

    [feat/#31] 이젼 평가 가져오기 api domain 모듈 추가

--- shared/domain/evaluate/src/commonMain/kotlin/com/kus/shared/domain/evaluate/di/evaluateDomainModule.kt
+++ shared/domain/evaluate/src/commonMain/kotlin/com/kus/shared/domain/evaluate/di/evaluateDomainModule.kt
@@ -1,9 +1,11 @@
 package com.kus.shared.domain.evaluate.di
 
+import com.kus.shared.domain.evaluate.usecase.GetEvaluationUseCase
 import com.kus.shared.domain.evaluate.usecase.PostEvaluationUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val evaluateDomainModule = module {
+    singleOf(::GetEvaluationUseCase)
     singleOf(::PostEvaluationUseCase)
 }
--- shared/domain/evaluate/src/commonMain/kotlin/com/kus/shared/domain/evaluate/repository/EvaluateRepository.kt
+++ shared/domain/evaluate/src/commonMain/kotlin/com/kus/shared/domain/evaluate/repository/EvaluateRepository.kt
@@ -1,6 +1,10 @@
 package com.kus.shared.domain.evaluate.repository
 
+import com.kus.shared.domain.model.evaluate.PreviousEvaluation
+
 interface EvaluateRepository {
+    suspend fun getEvaluation(restaurantId: Long): PreviousEvaluation
+
     suspend fun postEvaluation(
         restaurantId: Long,
         evaluationScore: Double,
