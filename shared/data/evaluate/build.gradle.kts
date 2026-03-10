3fb681ca908e25049210bf998a5b0f5d48f7969d
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-03-11 00:20:59 +0900

    [add/#31] koin 의존성 추가

--- shared/data/evaluate/build.gradle.kts
+++ shared/data/evaluate/build.gradle.kts
@@ -46,6 +46,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
+                implementation(libs.koin.core)
                 implementation(libs.kotlinx.coroutines.core)
 
                 implementation(libs.kotlinx.serialization.json)
