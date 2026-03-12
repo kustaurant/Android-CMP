69671051418586d5a301a5eb40891d4e0188d62c
Chaerin Kim <rinring105@gmail.com>
2026-03-12 22:13:52 +0900

    [feat/#22] 스크랩 화면 api 연결 및 컴포넌트 연결

--- shared/feature/my/src/commonMain/kotlin/com/kus/feature/my/ui/community/MyScrapScreen.kt
+++ shared/feature/my/src/commonMain/kotlin/com/kus/feature/my/ui/community/MyScrapScreen.kt
@@ -1,12 +1,11 @@
 package com.kus.feature.my.ui.community
 
-import  UiState
+import UiState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
@@ -17,8 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kus.designsystem.theme.KusTheme
+import com.kus.designsystem.util.noRippleClickable
 import com.kus.feature.my.component.EmptyPage
 import com.kus.feature.my.component.MyPageTopBar
+import com.kus.feature.my.component.PostItem
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -66,15 +67,21 @@ internal fun MyScrapScreen(
                     }
 
                     item {
-                        Spacer(Modifier.height(30.dp))
+                        Spacer(Modifier.height(10.dp))
                     }
 
-                    itemsIndexed(restaurants) { index, item ->
-                        Box(
-                            modifier = Modifier.padding(horizontal = 20.dp)
-                        ) {
+                    itemsIndexed(
+                        items = restaurants,
+                        key = { _, item -> item.postId }
+                    ) { index, item ->
+                        PostItem(
+                            item = item,
+                            modifier = Modifier.noRippleClickable { onItemClick(item.postId) }
+                        )
+                    }
 
-                        }
+                    item {
+                        Spacer(Modifier.height(10.dp))
                     }
                 }
             }
