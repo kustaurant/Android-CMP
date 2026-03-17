c56c478861c05221ec1564657aed25d1397d71b6
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-03-17 23:59:08 +0900

    [feat/#44] 안드로이드 install splash 활성화 및 로고 추가

--- composeApp/src/androidMain/kotlin/com/kus/kustaurant/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kus/kustaurant/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge(
             statusBarStyle =
                 SystemBarStyle.light(
