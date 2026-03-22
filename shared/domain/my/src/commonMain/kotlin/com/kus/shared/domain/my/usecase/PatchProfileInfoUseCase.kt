4fbd69039e9015d718d2a859b362dcce4e133960
Chaerin Kim <rinring105@gmail.com>
2026-03-22 18:22:54 +0900

    [feat/#45] repository 및 usecase 구현

---
+++ shared/domain/my/src/commonMain/kotlin/com/kus/shared/domain/my/usecase/PatchProfileInfoUseCase.kt
@@ -0,0 +1,14 @@
+package com.kus.shared.domain.my.usecase
+
+import MyRepository
+
+class PatchProfileInfoUseCase(
+    private val repository: MyRepository,
+) {
+    suspend operator fun invoke(
+        nickname: String,
+        phoneNumber: String,
+    ): String {
+        return repository.patchProfileInfo(nickname, phoneNumber)
+    }
+}
