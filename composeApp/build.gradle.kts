33744b095e42936bc425348f9c2bb81f128a5a3c
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2026-04-04 00:19:21 +0900

    [BUILD] versionCode 변경

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -139,7 +139,7 @@ android {
         applicationId = "com.kust.kustaurant"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 30
+        versionCode = 31
         versionName = "26.4.3"
     }
     packaging {
